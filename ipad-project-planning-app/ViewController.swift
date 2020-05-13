6c6f1bfc52022d95bf35510f56345025c4c0fcb0
stelan.slt@gmail.com <stelan@578951>
2020-05-13 12:49:47 +0530

    Initial Commit

---
+++ ipad-project-planning-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ipad-project-planning-app
+//
+//  Created by Stelan Simonsz on 5/13/20.
+//  Copyright © 2020 Stelan Simonsz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
