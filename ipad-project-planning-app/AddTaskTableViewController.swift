b6a703038bbf316a6d270f7543eb77490a5d9459
stelan.slt@gmail.com <stelan@578951>
2020-05-14 13:24:45 +0530

    add task popover added.

---
+++ ipad-project-planning-app/AddTaskTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AddTaskTableViewController.swift
+//  ipad-project-planning-app
+//
+//  Created by Stelan Simonsz on 5/14/20.
+//  Copyright © 2020 Stelan Simonsz. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
+extension AddTaskTableViewController {
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 1 && indexPath.row == 0 {
+            return 200.0
+        }
+        // Make Notes text view bigger: 80
+        if indexPath.section == 0 && indexPath.row == 0 {
+            return 250.0
+        }
+        
+        return 0
+    }
+    
+}
